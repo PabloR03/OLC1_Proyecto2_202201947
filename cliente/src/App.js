141bcea5bc0f950649ceb58c6f78257acb2d9da4
Pablo Rodríguez Lima <pabloa10rodriguez@gmail.com>
2024-04-08 22:09:40 -0600

    front terminado, solo faltan reportes

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -35,49 +35,120 @@ function App() {
   }
 
   const CargarArchivo = (event) => {
-    var file = event.target.files[0];
-    var reader = new FileReader();
-    reader.onload = function (event) {
-      var contents = event.target.result;
-      editorRef.current.setValue(contents);
-    };
-    reader.readAsText(file);
-  }
+    const file = event.target.files[0];
+    
+    // Verificar si el archivo tiene la extensión .sc
+    if (file.name.endsWith('.sc')) {
+      const reader = new FileReader();
+      reader.onload = function (event) {
+        const contents = event.target.result;
+        editorRef.current.setValue(contents);
+      };
+      reader.readAsText(file);
+    } else {
+      alert('Por favor seleccione un archivo con extensión .sc');
+      // Puedes agregar más lógica aquí si lo deseas, como limpiar el input de archivo o mostrar un mensaje de error
+    }
+  };
+  
+  const GuardarComoArchivo = () => {
+    try {
+        const contenido = editorRef.current.getValue(); // Obtener el contenido del editor
+        const blob = new Blob([contenido], { type: 'text/plain' }); // Crear un Blob con el contenido en formato texto
+        const url = URL.createObjectURL(blob); // Crear una URL del Blob
+        
+        const a = document.createElement('a'); // Crear un elemento <a> para el enlace de descarga
+        a.href = url; // Establecer la URL como href del elemento <a>
+        a.download = 'archivo.sc'; // Establecer el nombre de archivo con extensión .sc
+        
+        // Simular un clic en el enlace
+        a.click(); 
+        
+        // Liberar la URL del Blob
+        URL.revokeObjectURL(url); 
+    } catch (error) {
+        alert("Error al guardar el archivo: " + error);
+    }
+};
+
+  const CrearArchivo = () => {
+    try {
+      editorRef.current.setValue(''); // Limpiar el contenido del editor de texto
+      alert("Se ha creado un nuevo archivo. Puede comenzar a escribir.");
+    } catch (error) {
+      alert("Error al crear un nuevo archivo: " + error);
+    }
+  };
+  
 
 
   return (
     <div className="App">
-      <div class='text-center'>
-        <h1>Proyecto 2 - OLC1</h1>
+      <div className="text-center">
+        <h3>CompiScript+ 202201947</h3>
       </div>
-      <br></br>
-      <div class='text-center'>
-        <div class="container">
-          <div class="row">
-            <input type="file" id="file" class="form-control form-control-lg" onChange={CargarArchivo} />
-          </div>
-          <br></br>
-          <div class="row">
-            <input type="button" value="Interpretar" id="btnCargar" class="form-control form-control-lg" onClick={interpretar} />
+      <br />
+      <div className="text-center">
+        <div className="container">
+          <div className="row">
+            {/* Primer botón desplegable */}
+            <div className="col">
+              <div className="dropdown">
+                <button className="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
+                  Archivo
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li><label className="dropdown-item">
+                    Nuevo Archivo
+                    <input type="file" style={{ display: "none" }} onChange={CrearArchivo} />
+                  </label></li>
+                  <li><label className="dropdown-item">
+                    Abrir Archivo
+                    <input type="file" style={{ display: "none" }} onChange={CargarArchivo} />
+                  </label></li>
+                  <li><label className="dropdown-item">
+                    Guardar Archivo
+                    <input type="file" style={{ display: "none" }} onChange={GuardarComoArchivo} />
+                  </label></li>
+                </ul>
+              </div>
+            </div>
+            {/* Tercer botón desplegable */}
+            <div className="col">
+              <div className="dropdown">
+                <button className="btn btn-secondary btn-lg dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
+                  Reportes
+                </button>
+                <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
+                  <li><a className="dropdown-item" href="#">Reporte de Errores</a></li>
+                  <li><a className="dropdown-item" href="#">Generar Arbol AST</a></li>
+                  <li><a className="dropdown-item" href="#">Reporte Tabla de Simbolos</a></li>
+                </ul>
+              </div>
+            </div>
+            {/* Botón de interpretar */}
+            <div className="col">
+              <button type="button" className="btn btn-success btn-lg" onClick={interpretar}>Ejecutar</button>
+            </div>
           </div>
         </div>
       </div>
-      <br></br>
-      <div class='text-center style={{ height: "80%", width: "80%" }} '>
-        <div class="container" >
-          <div class="row">
-            <div class="col">
-              <p>Entrada</p>
+      <br />
+      {/* Tercer contenedor con área de consola */}
+      <div className="text-center">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col">
+              <p className="text-center">Entrada</p>
               <Editor height="90vh" defaultLanguage="java" defaultValue="" theme="vs-dark" onMount={(editor) => handleEditorDidMount(editor, "editor")} />
             </div>
-            <div class="col">
-              <p>Consola</p>
+            <div className="col">
+              <p className="text-center">Consola</p>
               <Editor height="90vh" defaultLanguage="cpp" defaultValue="" theme="vs-dark" options={{ readOnly: true }} onMount={(editor) => handleEditorDidMount(editor, "consola")} />
             </div>
           </div>
         </div>
       </div>
-
     </div>
   );
 }
